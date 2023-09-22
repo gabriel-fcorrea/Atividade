adfd0d6cbd61762933e6345683d6ba17ac47dba8
gabriel-fcorrea <gabrecorrea@gmail.com>
2023-09-21 21:33:50 -0300

    version 1.6

---
+++ app/src/main/java/br/com/etecia/myapp/Login2Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login2_layout);
+    }
+}
